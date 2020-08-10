22478dce4b88049c7837b70d85b6c7fd34b665d4
sadfa <sadfa@bots.github.com>
2020-08-10 10:04:14 +0000

    Use tmp to copy contents from file_store branch

--- run_site_checker.sh
+++ run_site_checker.sh
@@ -14,18 +14,21 @@ git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
 git checkout "$file_store_branch"
 
-cp -r ./contents .
+mkdir ./tmp/
+cp -r ./contents ./tmp
 
 git checkout "$main_branch"
-
+mv ./tmp/contents/ .
 
 python3 website_checker.py
+cp -r ./contents/ ./tmp/
 
 git checkout "$file_store_branch"
-
-git add contents
+cp -r ./tmp/contents .
+rm -rf ./tmp/
 
 git add .
+
 set +e  # Grep succeeds with nonzero exit codes to show results.
 
 if git status | grep 'new file\|modified'
@@ -38,5 +41,7 @@ else
     set -e
     echo "No changes since last run"
 fi
+
+git checkout "$main_branch"
 
 echo "finish"
