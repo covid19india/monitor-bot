44a08305484f514113b3d7a6aa4081093c46f13d
root <root@ubuntu-learning.us-central1-a.c.covid19indiaorg.internal>
2020-08-10 07:14:22 +0000

    Add website checker

---
+++ website_checker.py
@@ -0,0 +1,145 @@
+import requests
+from bs4 import BeautifulSoup
+
+from datetime import datetime
+import pytz
+import os
+import shutil
+
+import difflib
+
+
+def check_now(a,urls):
+	url = urls[a]
+	# Make a new folder
+	os.makedirs('./contents/new/',exist_ok=True)
+	# Setup old and new path
+	old_file_path = "./contents/old/" + url["name"]
+	new_file_path = "./contents/new/" + url["name"]
+	# Fetch latest version of the site
+	try:
+		res_new = requests.get(url["url"], timeout=10).text
+		soup_new = BeautifulSoup(res_new, 'html.parser')
+	except Exception as e:
+		print(e)
+		return
+	# Save new version to file
+	with open(new_file_path, "w") as new:
+		new.write(str(soup_new))
+	# See if an old version is present
+	try:
+		old = open(old_file_path, "r")
+	except:
+		print("Old copy not found. Continue")
+		return
+	# Open the new version again for comparison
+	new = open(new_file_path, "r")
+	# Use difflib to find differences between old and new
+	diff = difflib.unified_diff(
+		old.readlines(),
+		new.readlines(),
+		fromfile=old_file_path,
+		tofile=new_file_path
+		)
+	# If there is a difference, change status from Same to Updated
+	status = "Same"
+	for line in diff:
+		if url['check'] in line:
+			print("Content changed for: "+url["name"])
+			print(line)
+			status = "Updated"
+			break
+	
+	print(
+		"Status ["
+		+ url["name"]
+		+ "] : " 
+		+ status
+		)
+	# Close open new file
+	new.close()
+	
+	return status
+	
+	
+if __name__ == "__main__":
+	'''
+	Code to check if a URL has updated
+	'''
+	# For KL
+	today = datetime.now(pytz.timezone('Asia/Calcutta')).strftime('%d/%m/%Y')
+
+	# Setup URLS
+	urls = [
+		{
+			"name" : "AndamanandNicobarIslands",
+			"url" : "https://dhs.andaman.gov.in",
+			"check" : 'ISLANDS HEALTH BULLETIN FOR CONTAINMENT OF COVID-19'
+		},
+		{
+			"name" : "Assam" ,
+			"url" : "https://covid19.assam.gov.in/all-districts/",
+			"check" : ''
+		},
+		{
+			"name" : "Chandigarh",
+			"url" : "http://chdcovid19.in/",
+			"check" : '<div class="col-md-3 form-group">'
+		},
+		{
+			"name" : "DadraandNagarHaveliandDamanandDiu",
+			"url" : "https://dddcovid19.in/index",
+			"check": 'ContentPlaceHolder1_lbldate'
+		},
+		{
+			"name" : "Haryana",
+			"url" : "http://www.nhmharyana.gov.in/page.aspx?id=208",
+			"check" : '<a href="/WriteReadData/userfiles/file/'
+		},
+		{
+			"name" : "Kerala",
+			"url" : "http://dhs.kerala.gov.in/%E0%B4%A1%E0%B5%86%E0%B4%AF%E0%B4%BF%E0%B4%B2%E0%B4%BF-%E0%B4%AC%E0%B5%81%E0%B4%B3%E0%B5%8D%E0%B4%B3%E0%B4%B1%E0%B5%8D%E0%B4%B1%E0%B4%BF%E0%B4%A8%E0%B5%8D%E2%80%8D/",
+			"check":'title="' + today + '"'
+		},
+		{
+			"name" : "Puducherry",
+			"url" : "https://health.py.gov.in/",
+			"check": 'health.py.gov.in/sites/default/files/'
+		},
+		{
+			"name" : "Tripura",
+			"url" : "https://covid19.tripura.gov.in",
+			"check": 'lblActiceCases'
+		},
+		{
+			"name" : "WestBengal",
+			"url" : "https://www.wbhealth.gov.in/pages/corona/bulletin",
+			"check":'<tbody>'
+		}
+	]
+	#Setup BOT TOKEN and CHAT ID from environment variable
+	MONITOR_BOT_TOKEN = os.environ['MONITOR_BOT_TOKEN']
+	CHAT_ID = "16027374"
+
+
+	for a in range(len(urls)):
+		status = check_now(a,urls)
+		if status == "Updated":
+				message_call = (
+					"https://api.telegram.org/bot"
+					+ MONITOR_BOT_TOKEN
+					+ "/sendMessage?chat_id="
+					+ CHAT_ID
+					+ "&text="
+					+ urls[a]['name']
+					+ " site got updated!\n\n"
+					+ "URL: "
+					+ urls[a]['url']
+					)
+				requests.get(message_call)
+	# Make the new contents as old
+	try:
+		shutil.rmtree('./contents/old/')
+	except FileNotFoundError:
+		pass
+	os.rename('./contents/new/','./contents/old/')
