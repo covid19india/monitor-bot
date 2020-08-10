6b1790ed7c316a6dc54008564fed8ac93fdbec40
root <root@ubuntu-learning.us-central1-a.c.covid19indiaorg.internal>
2020-08-10 07:53:11 +0000

    Shift to shell

---
+++ run_site_checker.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eu
+
+repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_name="origin"
+main_branch="master"
+file_store_branch="file_store"
+
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "${GITHUB_ACTOR}@bots.github.com"
+
+
+git checkout "$file_store_branch"
+
+cp -r ./contents .
+
+git checkout "$main_branch"
+
+
+python3 website_checker.py
+
+git checkout "$file_store_branch"
+
+git add contents
+
+git add .
+set +e  # Grep succeeds with nonzero exit codes to show results.
+
+if git status | grep 'new file\|modified'
+then
+    set -e
+    git commit -am "data updated on - $(date)"
+    git remote set-url "$remote_name" "$repo_uri" # includes access token
+    git push --force-with-lease "$remote_name" "$file_store_branch"
+else
+    set -e
+    echo "No changes since last run"
+fi
+
+echo "finish"
