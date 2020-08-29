10ef0126a6148e6abb7263552e7b8b41ad6d3347
harsshjain <65491327+harsshjain@users.noreply.github.com>
2020-08-29 21:29:07 +0530

    Update twitter_notifications.py

--- twitter_notifications.py
+++ twitter_notifications.py
@@ -91,7 +91,7 @@ def stream_tweets(api, results, bot, chat_id_data_ops, chat_id_aux, minutes=10):
             for status in tweepy.Cursor(
                 api.search,
                 q=result["searchcriteria"],
-                count=10,
+                count=15,
                 result="recent",
                 include_entities=True,
             ).items():
